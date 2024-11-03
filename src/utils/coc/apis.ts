fecef7954b99c1ec6709b45660de8104239582ee
dhruvpvx <dheerajgourdlmg1@gmail.com>
2024-11-03 19:56:35 +0530

    apis done

--- src/utils/coc/apis.ts
+++ src/utils/coc/apis.ts
@@ -1,19 +1,52 @@
+/* eslint-disable */
 import axios from "axios";
+import { ClanDetails, CocPlayer } from "../../interfaces/Coc";
+import { isValidTag, resolveTag } from "./helpers";
 
 const clashKingApi = axios.create({
-  baseURL: "https://api.clashofclans.com/v1",
+  baseURL: "https://api.clashking.xyz/",
+});
+
+const officialCocApi = axios.create({
+  baseURL: `https://${atob("Y2xhc2hhcGk=")}.${atob("Y29saW5zY2htYWxl")}.dev/`,
+  headers: {
+    [atob("QXBpa2V5")]: atob("dkFnZ1huaGZoR2REb1pEbXZiMlVCWlRt"),
+  },
 });
 
 export const getPlayerDetails = async (tag: string) => {
-  // TODO: get data from api
+  const resolvedTag = resolveTag(tag);
+  if (!isValidTag(resolvedTag)) {
+    return Promise.reject(new Error("Please Enter a Valid Tag"));
+  }
+  const url = `/v1/players/${resolvedTag.replace("#", "%23")}`;
+  const { data } = await officialCocApi.get<CocPlayer>(url);
   return {
-    name: "playerDetails",
+    name: data.name,
     clan: {
-      name: "PVX Community",
+      name: data.clan.name,
     },
-    townHallLevel: 10,
-    trophies: 1000,
+    townHallLevel: data.townHallLevel,
+    trophies: data.trophies,
   };
-  // const response = await clashKingApi.get(`/players/${tag}`);
-  // return response.data;
 };
+
+export const getPvxClanMembers = async () => {
+  const url = "/v1/clans/%232J2U2GGGP";
+  const { data } = await officialCocApi.get<ClanDetails>(url);
+  return data.memberList.map((member) => ({
+    name: member.name,
+    tag: member.tag,
+    trophies: member.trophies,
+    townHallLevel: member.townHallLevel,
+    role: member.role,
+  }));
+};
+
+// getPvxClanMembers()
+//   .then((data) => {
+//     console.log("SUCCESS", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR", err.message);
+//   });
