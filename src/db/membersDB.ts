0929f58af23cf7bfc315b68bd4eeb6f34110cc30
Shubham Rawat <sr856161@gmail.com>
2025-10-15 13:01:17 +0530

    feat: fetch random memberjid to update lid

--- src/db/membersDB.ts
+++ src/db/membersDB.ts
@@ -61,7 +61,7 @@ export const updateMemberLIDsFromSignal = async (bot: Bot) => {
   try {
     // 1️⃣ Fetch ~100 members from DB whose lid is 'unknown'
     const res = await pool.query(
-      "SELECT memberjid FROM member WHERE lid = 'unknown' LIMIT 1;"
+      "SELECT memberjid FROM member WHERE lid = 'unknown' ORDER BY RANDOM() LIMIT 1;"
     );
     const membersToUpdate: string[] = res.rows.map((r) => r.memberjid);
 
